77af8deb394934b8255439b4887689fe848d676c
Ramil Safin <ramilsafnab1996@gmail.com>
2025-06-25 01:14:20 +0300

    Refactor: add target_link_with_sanitizers() helper

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -11,5 +11,12 @@ if(CMAKE_BUILD_TYPE STREQUAL "Sanitize")
   target_compile_options(sanitizers INTERFACE ${SANITIZER_FLAGS})
   target_link_options(sanitizers INTERFACE ${SANITIZER_FLAGS})
 
-  message(STATUS "Sanitize build: enabled with flags: ${SANITIZER_FLAGS}")
+  message(STATUS "✅ Sanitize build: enabled with flags: ${SANITIZER_FLAGS}")
 endif()
+
+function(target_link_with_sanitizers target)
+  if(TARGET sanitizers)
+    message(STATUS "✅ Enabled sanitizers for target ${target}")
+    target_link_libraries(${target} PRIVATE sanitizers)
+  endif()
+endfunction()

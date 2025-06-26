d858449ab129e5475385174107f2e6c9340f484c
Ramil Safin <ramilsafnab1996@gmail.com>
2025-06-27 01:06:33 +0300

    Add Doxygen docs generation + CI workflow

--- include/math/arithmetic.hpp
+++ include/math/arithmetic.hpp
@@ -1,10 +1,30 @@
 #ifndef MATH_ARITHMETIC_HPP
 #define MATH_ARITHMETIC_HPP
 
+/**
+ * @file arithmetic.hpp
+ * @brief Arithmetic operations library (add, subtract).
+ */
+
 namespace math
 {
 
+/**
+ * @brief Adds two integers.
+ *
+ * @param a First integer.
+ * @param b Second integer.
+ * @return Sum of a and b.
+ */
 int add(int a, int b);
+
+/**
+ * @brief Subtracts two integers.
+ *
+ * @param a First integer.
+ * @param b Second integer.
+ * @return Difference of a and b.
+ */
 int subtract(int a, int b);
 
 }  // namespace math

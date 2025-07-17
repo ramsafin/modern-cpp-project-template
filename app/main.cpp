a0e82e6f4c02f24f8fc1bc2d2ed5531fe48635a6
Ramil Safin <ramilsafnab1996@gmail.com>
2025-07-18 00:33:00 +0300

    Add spdlog demo app example

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,25 @@
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
 #include <iostream>
 
 #include "modern_cpp/library.hpp"
 
 int main() {
+  auto console = spdlog::stdout_color_mt("console");
+  spdlog::set_level(spdlog::level::debug);
+
   const int left = 10;
   const int right = 4;
 
-  std::cout << "Add: " << modern_cpp::add(left, right) << '\n';
-  std::cout << "Subtract: " << modern_cpp::subtract(left, right) << '\n';
+  console->info("Calling add({}, {})", left, right);
+  const int sum = modern_cpp::add(left, right);
+  console->info("Result of add: {}", sum);
+
+  console->info("Calling subtract({}, {})", left, right);
+  const int diff = modern_cpp::subtract(left, right);
+  console->info("Result of subtract: {}", diff);
 
   return 0;
 }
